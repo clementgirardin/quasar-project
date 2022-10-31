79b5cfec054279664c6c1e5ffb76bc22592f6cd6
giracle <clement.girardin@divtec.ch>
2022-10-31 15:51:58 +0100

    Fin exercice 2 avec quelques details

--- src/pages/Exercice2Page.vue
+++ src/pages/Exercice2Page.vue
@@ -91,11 +91,50 @@
 <script>
 export default {
   name: 'Exercice2Page',
+
+  components: {
+    plat: require('components/Plat.vue').default
+  },
+
   data () {
     return {
       plats: [
-        // vos objets plats
+        {
+          id: 1,
+          image: 'https://i.imgur.com/0umadnY.jpg',
+          nom: 'Burger',
+          description: "Un hamburger est un sandwich composé d'une ou plusieurs tranches de viande hachée, généralement du bœuf, placées dans un petit pain ou une brioche.",
+          note: 4
+        },
+        {
+          id: 2,
+          image: 'https://i.imgur.com/b9zDbyb.jpg',
+          nom: 'Pizza',
+          description: "La pizza est un plat savoureux d'origine italienne, consistant en une base généralement ronde et aplatie de pâte levée à base de blé.",
+          note: 5
+        },
+        {
+          id: 3,
+          image: 'https://i.imgur.com/0umadnY.jpg',
+          nom: 'Burger',
+          description: "Un hamburger est un sandwich composé d'une ou plusieurs tranches de viande hachée, généralement du bœuf, placées dans un petit pain ou une brioche.",
+          note: 1
+        },
+        {
+          id: 4,
+          image: 'https://i.imgur.com/xAuhNVg.jpg',
+          nom: 'BBQ Ribs',
+          description: 'Les BBQ ribs ou barbecue ribs sont des grands classiques très appréciés partout dans le monde.',
+          note: 5
+        }
       ]
+    }
+  },
+  methods: {
+    supprimerPlat (id) {
+      this.plats = this.plats.filter(function (plat) {
+        return plat.id !== id
+      })
     }
   }
 }
